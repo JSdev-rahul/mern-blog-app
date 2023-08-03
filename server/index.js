1c7914b67b8a4509ec11454718fff6a8bbaa982a
rahul <rahul@gmail.com>
2023-08-03 14:50:28 +0530

    pm2 add

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const morgon=require("morgan")
 const route = require("./routes/route.js");
 const Connection = require("./database/db.js");
 const app = express();
@@ -10,8 +10,9 @@ const path = require("path");
 app.use(express.json());
 app.use( express.static('uploads/'));
 app.use(cors());
+app.use(morgon('dev'))
 Connection();
-
+console.log("runn")
 app.use("/", route);
 
 app.listen(PORT, () => {
